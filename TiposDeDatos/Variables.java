52e86d9c1e8f13b65ee0cd3411ad34a9c0db8626
Nicolás Guzmán <n3kguzman@gmail.com>
2024-08-27 19:33:53 -0400

    first

---
+++ TiposDeDatos/Variables.java
@@ -0,0 +1,16 @@
+package TiposDeDatos;
+
+public class Variables {
+    
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        String nombre = "Nicolás";
+        char letra = 'H';
+        double temperatura = 15.5;
+        boolean siOno = false;
+        long largo = 123456789;
+        
+
+    }
+}
